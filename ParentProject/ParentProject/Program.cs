04fa3e1f8bbe5eb3417a03c872361b4b5ec90d56
nikolay.galushka <nikolay.galushka@betlab.com>
2019-02-22 11:25:15 +0200

    initial

---
+++ ParentProject/ParentProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParentProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
